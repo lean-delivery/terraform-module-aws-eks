506c03803f19d483648992b4c268a1727767c23a
Yauheni Anashkin <yauheni_anashkin@epam.com>
2019-04-04 19:08:46 +0300

    add simple test

---
+++ test/tf-module-aws-eks_test.go
@@ -0,0 +1,50 @@
+package test
+
+import (
+	"testing"
+	// "fmt"
+	"time"
+    "math/rand"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	// "github.com/stretchr/testify/assert"
+)
+
+var terraformDirectory = "../examples"
+var region             = "us-east-1"
+var account            = ""
+var region             = "eu-west-1"
+var vpc_id             = "vpc-49fb682f"
+var subnets_id         = []string{"subnet-8d79a2eb", "subnet-938b23db", "subnet-ecfc19b6"}
+
+func Test(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: terraformDirectory,
+
+		Vars: map[string]interface{}{
+			"aws_region": region,
+			"name": "Test_EKS_name_" + randSeq(10)
+			"vpc_id": vpc_id,
+			"subnets_id": subnets_id,
+			"ami_id": "ami-01e08d22b9439c15a",
+  			"instance_type": "m4.large",
+  			"asg_max_size": "10",
+  			"spot_price": "0.05",
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.Init(t, terraformOptions)
+	terraform.Apply(t, terraformOptions)
+	account = terraform.Output(t, terraformOptions, "account_id")
+}
+
+func randSeq(n int) string {
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
+}
